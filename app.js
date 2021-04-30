83d98d64ef878748ddcaa88c4cf62a16abafd51f
harshal-verma <harshalvermaaaaa@gmail.com>
2021-04-30 22:14:38 +0530

    updated: app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+let tasks = [{}];
+const addTaskInputBox = document.getElementById("add-task");
+const tasksList = document.getElementById("list");
+
+function addTodo(task){
+    tasks.push(task);
+    renderList();
+}
+
+function deleteTodo(){
+
+}
+
+function renderList(){
+
+}
+
+function checkTodo(){
+
+}
+
+console.log("sejfsjfsfjsj")
