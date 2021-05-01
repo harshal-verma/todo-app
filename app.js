41a1e300e8d05c2f975ed2dcde6b73bdc87f1434
harshal-verma <harshalvermaaaaa@gmail.com>
2021-05-01 13:03:13 +0530

    updated app.js

--- app.js
+++ app.js
@@ -1,21 +1,43 @@
-let tasks = [{}];
+let tasks = [
+    {
+    done: false,
+    text: 'gym',
+    id: 1
+    },
+];
 const addTaskInputBox = document.getElementById("add-task");
 const tasksList = document.getElementById("list");
 
-function addTodo(task){
+function addTodo(task) {
     tasks.push(task);
     renderList();
 }
 
-function deleteTodo(){
-
+function deleteTodo(taskId) {
+    const newTask = tasks.filter(function (task) {
+        return task.id !== taskId;
+    });
+    tasks = newTask;
+    renderList();
 }
 
-function renderList(){
+function renderList() {
+    for(let i = 0; i< tasks.length; i++){
+        const li = document.createElement('li');
 
+        li = `
+        <li>
+        <input type="checkbox" id="task1">
+        <label for="task1">
+            go to gym
+        </label>
+        <button>delete</button>
+        </li>
+        `;
+    }
 }
 
-function checkTodo(){
+function checkTodo() {
 
 }
 
